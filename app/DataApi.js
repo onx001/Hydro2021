8ca2bb22b14808edeb2f4547d26ec62fdbc53ece
tanguoyang <64419996+tanguoyang@users.noreply.github.com>
2021-06-16 20:39:34 +0800

    Implement WiFi Library

--- app/DataApi.js
+++ app/DataApi.js
@@ -4,7 +4,7 @@ import Readline from '@serialport/parser-readline'
 import Ready from '@serialport/parser-ready'
 
 export const writeStringToSerialPort = (text,callback) => {
-    const port = new SerialPort("COM3", { //open up a port to kit
+    const port = new SerialPort("COM4", { //open up a port to kit
         baudRate: 115200,
         // autoOpen:false
     });
@@ -13,12 +13,16 @@ export const writeStringToSerialPort = (text,callback) => {
 
     const readyParser = port.pipe(new Ready({delimiter:'READY'}))
 
+    readlineParser.on('data',(line) => {
+        console.log(line)
+    })
+
     readyParser.on('ready', () => { //run this function after ready byte is received
         console.log('the ready byte sequence has been received. Arduino is ready.')
         port.write(text)
         port.drain(() => {
             callback()
-            port.close()//close the port once all done
+            // port.close()//close the port once all done
         })
         // port.close()
     })
